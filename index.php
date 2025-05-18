0af4d534b3b7c5bc7d8594ae52c0ae99fabf9186
GhChrist13 <ctasseu13@gmail.com>
2025-05-18 10:53:40 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="index.css">
+    <title>Gestionnaire de stages</title>
+</head>
+
+<body>
+
+    <!-- Entête de la page============================================-->
+    <header>
+
+    </header>
+
+    <!-- Corps de page============================================== -->
+
+
+    <!-- contain========================================================================= -->
+
+    <div class="contain">
+
+        <!-- Block central========================= -->
+        <div class="central-block">
+
+            <!-- Barre de Recherche -->
+            <div class="search">
+                <input type="text" id="search" placeholder="Recherche"><span><i class="fa-solid fa-magnifying-glass"></i></span>
+            </div>
+
+            <!-- Filtrage -->
+            <div class="filtre">
+                <div class="filtres">ESIC</div>
+                <div class="filtres">ICSE</div>
+                <div class="filtres">CISE</div>
+                <div class="filtres">ESIC</div>
+                <div class="filtres">ICSE</div>
+                <div class="filtres">CISE</div>
+                <div class="filtres">ESIC</div>
+                <div class="filtres">ICSE</div>
+                <div class="filtres">CISE</div>
+            </div>
+
+            <!-- Block de stages -->
+
+            <div class="stage">
+
+                <div class="stages">
+                    <div class="images"></div>
+                    <div class="desc">
+                        <H2>Intitulé du stage</H2>
+                        <h5>Adresse de l'entreprise</h5>
+                        <h6 translate="no">Stage</h6>
+                        <span><i class="fa-solid fa-heart"></i></span>
+                        <button>Voir l'offre</button>
+                    </div>
+                </div>
+                <div class="stages">
+                    <div class="images"></div>
+                    <div class="desc">
+                        <H2>Intitulé du stage</H2>
+                        <h5>Adresse de l'entreprise</h5>
+                        <h6 translate="no">Stage</h6>
+                        <span><i class="fa-solid fa-heart"></i></span>
+                        <button>Voir l'offre</button>
+                    </div>
+                </div>
+                <div class="stages">
+                    <div class="images"></div>
+                    <div class="desc">
+                        <H2>Intitulé du stage</H2>
+                        <h5>Adresse de l'entreprise</h5>
+                        <h6 translate="no">Stage</h6>
+                        <span><i class="fa-solid fa-heart"></i></span>
+                        <button>Voir l'offre</button>
+                    </div>
+                </div>
+                <div class="stages">
+                    <div class="images"></div>
+                    <div class="desc">
+                        <H2>Intitulé du stage</H2>
+                        <h5>Adresse de l'entreprise</h5>
+                        <h6 translate="no">Stage</h6>
+                        <span><i class="fa-solid fa-heart"></i></span>
+                        <button>Voir l'offre</button>
+                    </div>
+                </div>
+                <div class="stages">
+                    <div class="images"></div>
+                    <div class="desc">
+                        <H2>Intitulé du stage</H2>
+                        <h5>Adresse de l'entreprise</h5>
+                        <h6 translate="no">Stage</h6>
+                        <span><i class="fa-solid fa-heart"></i></span>
+                        <button>Voir l'offre</button>
+                    </div>
+                </div>
+                <div class="stages">
+                    <div class="images"></div>
+                    <div class="desc">
+                        <H2>Intitulé du stage</H2>
+                        <h5>Adresse de l'entreprise</h5>
+                        <h6 translate="no">Stage</h6>
+                        <span><i class="fa-solid fa-heart"></i></span>
+                        <button>Voir l'offre</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Barre latérale======================== -->
+        <div class="side">
+  <div class="side-icons">
+    <div class="side-item">
+      <i class="fa-solid fa-bars" id="barre"></i><span class="hidden-desc">Menu</span>
+    </div>
+    <div class="side-item">
+      <i class="fa-solid fa-house"></i><span class="hidden-desc">Accueil</span>
+    </div>
+    <div class="side-item">
+      <i class="fa-solid fa-plus"></i><span class="hidden-desc">Ajouter une offre</span>
+    </div>
+    <div class="side-item">
+      <i class="fa-solid fa-heart"></i><span class="hidden-desc">Favoris</span>
+    </div>
+  </div>
+</div>
+
+
+    </div>
+
+    <script src="index.js"></script>
+</body>
+
+</html>
