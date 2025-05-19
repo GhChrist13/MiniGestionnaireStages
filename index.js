bfa6d949582c8470383c81aa1979f3c94d760df6
GhChrist13 <ctasseu13@gmail.com>
2025-05-19 10:50:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let centralBlock = document.querySelector('.central-block')
 let stage = document.querySelector('.stage')
 let side = document.querySelector('.side')
 let barre = document.getElementById('barre')
+let ajout_stage = document.getElementById('ajout_stage')
 let hiddenDesc = document.querySelectorAll('.hidden-desc')
 
 
@@ -13,4 +14,10 @@ barre.addEventListener('click', function () {
     
     side.classList.toggle('expand')
     if(side.classList.contains("expand")){stage.style = "grid-template-columns: 1fr 1fr;"}else{stage.style = "grid-template-columns: 1fr 1fr 1fr;"}
+})
+
+//ajout d'un stage
+
+ajout_stage.addEventListener('click', function(){
+    
 })
