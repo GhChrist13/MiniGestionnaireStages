842762840645ff95618aa8180d1ac998aeec2c6e
GhChrist13 <ctasseu13@gmail.com>
2025-05-20 18:55:48 +0200

    Create supprimer_stage.php

---
+++ supprimer_stage.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['id'])) {
+    $id = intval($_POST['id']);
+
+    $conn = new mysqli("localhost", "root", "", "offre_stage");
+    if ($conn->connect_error) {
+        die("Erreur de connexion : " . $conn->connect_error);
+    }
+
+    $sql = "DELETE FROM stage WHERE id_stage = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    
+    if ($stmt->execute()) {
+        echo "OK";
+    } else {
+        echo "Erreur";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
