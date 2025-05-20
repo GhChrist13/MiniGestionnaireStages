425e80b30592b3fa78e97d1b8fb377141e89180c
GhChrist13 <ctasseu13@gmail.com>
2025-05-20 18:53:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ let form_contain = document.querySelector('.form_contain')
 let hiddenDesc = document.querySelectorAll('.hidden-desc')
 let btn_annuler = document.querySelector('.btn-annuler')
 let overlay = document.querySelector('.overlay')
+let tb = document.getElementById('table_bord')
+let accueil = document.getElementById('accueil')
+let side_item = document.querySelectorAll('.side-item')
 
 
 // block latéral==================================================
@@ -16,6 +19,9 @@ let overlay = document.querySelector('.overlay')
 barre.addEventListener('click', function () {
     side.classList.toggle('expand')
     if(side.classList.contains("expand")){stage.style = "grid-template-columns: 1fr 1fr;"}else{stage.style = "grid-template-columns: 1fr 1fr 1fr;"}
+    side_item.forEach(side_item =>{
+        side_item.classList.toggle('exp')
+    })
 })
 
 //ajout d'un stage
@@ -23,3 +29,17 @@ barre.addEventListener('click', function () {
 overlay.style.display = "none"
 ajout_stage.addEventListener('click', function(){overlay.style = "display : block;"})
 btn_annuler.addEventListener('click', function(){overlay.style.display = "none"})
+overlay.addEventListener('click', function(e) {
+    if (e.target === overlay) {
+        overlay.style.display = "none";
+    }
+});
+
+
+//tableau de bord
+
+tb.addEventListener('click', function(){window.location.href = "tb.php"})
+
+// page accueil
+
+accueil.addEventListener('click', function(){window.location.href = "index.php"})
